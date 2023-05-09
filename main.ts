8bb7756262ca7b952d15473a2813d3d2cda6396e
ihch <nemu0sou@gmail.com>
2023-05-09 23:42:52 +0900

    feat: Support for handling path parameters

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
 import { HttpMethod } from "https://deno.land/std@0.186.0/http/method.ts";
 import { Handler, serveListener } from "https://deno.land/std@0.186.0/http/mod.ts";
+import { ConnInfo } from "https://deno.land/std@0.186.0/http/server.ts";
 import Logger from "https://deno.land/x/logger@v1.1.0/logger.ts";
+
+type AppRequest = Request & { routeParams?: { [key: string]: string } };
+type AppHandler = (req: AppRequest, connInfo: ConnInfo) => Response | Promise<Response>;
 
 class AppServer {
     private listener: Deno.Listener;
     private logger: Logger;
-    private routeTree: Node<Handler>;
+    private routeTree: Node<AppHandler>;
 
     constructor() {
         this.listener = Deno.listen({ port: 8080 });
         this.logger = new Logger();
-        this.routeTree = new Node<Handler>();
+        this.routeTree = new Node<AppHandler>();
     }
 
     async start() {
@@ -19,18 +23,21 @@ class AppServer {
         await serveListener(this.listener, (req, connInfo) => {
             const url = new URL(req.url);
             const path = url.pathname;
-            const params = url.searchParams;
+            const searchParams = url.searchParams;
 
-            this.logger.info("Request received", req.method, path, params.toString());
+            this.logger.info("Request received", req.method, path, searchParams.toString());
             
-            const handler = this.routeTree.find(path + '/' + req.method)
-            if (!handler) {
+            const node = this.routeTree.find(path + '/' + req.method)
+
+            if (!node || !node.handler) {
                 this.logger.error('No handler found.', req.method, path);
                 return new Response('404 Not found.', { status: 404 });
             }
             
+            const { handler, params: routeParams } = node;
+            
             try {
-                return handler(req, connInfo);
+                return handler(Object.assign(req, { routeParams }), connInfo);
             }
              catch (e) {
                 this.logger.error(e);
@@ -39,13 +46,22 @@ class AppServer {
         })
     }
     
-    add(path: string, method: HttpMethod, handler: Handler) {
+    add(path: string, method: HttpMethod, handler: AppHandler) {
         this.routeTree.insert(path + '/' + method, handler);
     }
     
-    get(path: string, handler: Handler) {
+    get(path: string, handler: AppHandler) {
         this.add(path, "GET", handler);
     }
+}
+
+const isPathParameterKeyword = (keyword: string) => {
+    // ':' で始まる文字列をパスパラメーターとして扱う
+    return keyword.startsWith(':');
+}
+
+const parsePathParameterKey = (keyword: string) => {
+    return keyword.substring(1);
 }
 
 interface Node<T> {
@@ -53,10 +69,19 @@ interface Node<T> {
     children: { [key: string]: Node<T> };
 }
 
+type Pattern = {
+    type: 'path' | 'regexp';
+    keyword: string;
+    path: string;
+}
+
 class Node<T> implements Node<T> {
+    private patterns: Pattern[];
+
     constructor() {
         this.value = null;
         this.children = {};
+        this.patterns = [];
     }
     
     insert(path: string, value: T) {
@@ -66,6 +91,16 @@ class Node<T> implements Node<T> {
         let currentNode: Node<T> = this;
 
         for (const pathPart of pathArray) {
+            if (isPathParameterKeyword(pathPart)) {
+                const keyword = parsePathParameterKey(pathPart);
+                const pattern: Pattern = {
+                    type: 'path',
+                    keyword,
+                    path: pathPart,
+                };
+                currentNode.patterns.push(pattern);
+            }
+
             if (!currentNode.children[pathPart]) {
                 currentNode.children[pathPart] = new Node<T>();
             }
@@ -75,20 +110,41 @@ class Node<T> implements Node<T> {
         currentNode.value = value;
     }
     
-    find(path: string): T | null {
+    find(path: string): { handler: T | null, params: { [key: string]: string } } | null {
         const pathArray = splitPath(path);
         
         // deno-lint-ignore no-this-alias
         let currentNode: Node<T> = this;
+        const params: { [key: string]: string } = {};
 
         for (const pathPart of pathArray) {
-            if (!currentNode.children[pathPart]) {
-                currentNode.children[pathPart] = new Node<T>();
+            if (currentNode.children[pathPart]) {
+                currentNode = currentNode.children[pathPart];
+                continue;
             }
-            currentNode = currentNode.children[pathPart];
+
+            for (const pattern of currentNode.patterns) {
+                /*
+                TODO: 複数のパスパラメーターパターンがある場合の処理
+                今はパターンの一つ目を返す実装になっている. 
+
+                例
+                    同じ階層で別のパスパラメーターが存在するみたいなの
+                    app.get('/:username', (req) => { res.send('OK') });
+                    app.get('/:photos/:id, (req) => { res.send('OK') });
+                */
+                if (pattern.type === 'path') {
+                    currentNode = currentNode.children[pattern.path];
+                    params[pattern.keyword] = pathPart;
+                    break;
+                }
+            }
         }
         
-        return currentNode.value;
+        return { 
+            handler: currentNode.value,
+            params,
+        };
     }
 }
 
@@ -97,13 +153,55 @@ const splitPath = (path: string): string[] => {
     return pathArray;
 }
 
+/*
+
+function main() {
+    const app = new AppServer();
+    
+    app.add('/', app.Method.GET, (req, res) => { res.send('Hello World') });
+
+    app.get('/echo', (req, res) => { res.send('OK') });
+
+    app.get('/photos/:id', (req, res) => { res.send('OK') });
+
+    app.get('/:users/photos/:id', (req, res) => { res.send('OK') });
+    
+    app.get('/health', (req, res) => { res.send('OK') });
+    
+    app.start();
+}
+
+*/
+
 const app = new AppServer();
 
 app.get('/', () => new Response('Hello World', { status: 200 }));
+
 app.get('/ping', () => new Response('pong', { status: 200 }));
+
 app.get('/echo', (req) => {
     const params = new URL(req.url).searchParams;
     return new Response(params.get('say') ?? 'hello', { status: 200 });
+});
+
+app.get('/mynameis/:name', (req) => {
+    const routeParams = req.routeParams ?? {};
+    return new Response(routeParams.name ?? 'hello', { status: 200 });
+});
+
+app.get('/mynameis/:name/homete', (req) => {
+    const routeParams = req.routeParams ?? {};
+    return new Response(routeParams.name + ' is great.' ?? 'hello', { status: 200 });
+});
+
+app.get('/mynameis/:name/homete/:word', (req) => {
+    const routeParams = req.routeParams ?? {};
+    return new Response(routeParams.name + ' is ' + routeParams.word + '.' ?? 'hello', { status: 200 });
+});
+
+app.get('/:word', (req) => {
+    const routeParams = req.routeParams ?? {};
+    return new Response(routeParams.word ?? 'hello', { status: 200 });
 });
 
 await app.start();
